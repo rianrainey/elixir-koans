7d0f8e02ef86c4fe665bcd2f004a4d0e44709d13
Rian Rainey <rrainey@covermymeds.com>
2016-06-05 13:31:34 -0500

    Add number thoughts

--- lib/koans/04_numbers.ex
+++ lib/koans/04_numbers.ex
@@ -14,79 +14,79 @@ defmodule Numbers do
 
   koan "Let's grab the individual digits in a list" do
     individual_digits = Integer.digits(58127)
-    assert individual_digits == ___
+    assert individual_digits == [5, 8, 1, 2, 7]
   end
 
   koan "Oh no! I need it back together" do
     number = Integer.undigits([1, 2, 3, 4])
 
-    assert number == ___
+    assert number == 1234
   end
 
   koan "I think I need my number as a char" do
     char_digit = Integer.to_char_list(7)
 
-    assert char_digit == ___
+    assert char_digit == '7'
   end
 
   koan "Actually I want my number as a string" do
     string_digit = Integer.to_string(1234)
 
-    assert string_digit == ___
+    assert string_digit == "1234"
   end
 
   koan "The meaning of life in hexidecimal is 2A!" do
-    assert Integer.parse("2A", 16) == {___, ""}
+    assert Integer.parse("2A", 16) == {42, ""}
   end
 
   koan "I don't want this decimal point, let's round up" do
-    assert Float.ceil(34.25) == ___
+    assert Float.ceil(34.25) == 35
   end
 
   koan "OK, I only want it to 1 decimal place" do
-    assert Float.ceil(34.25, 1) == ___
+    assert Float.ceil(34.25, 1) == 34.3
   end
 
   koan "Rounding down is what I need" do
-    assert Float.floor(99.99) == ___
+    assert Float.floor(99.99) == 99
   end
 
   koan "Rounding down to 2 decimal places" do
-    assert Float.floor(12.345, 2) == ___
+    assert Float.floor(12.345, 2) == 12.34
   end
 
   koan "Just want to parse to a float" do
-    assert Float.parse("34.5") == {___, ""}
+    assert Float.parse("34.5") == {34.5, ""}
   end
 
   koan "Hmm, I want to parse this but it has some strings" do
-    assert Float.parse("1 million dollars") == {___, " million dollars"}
+    assert Float.parse("1 million dollars") == {1.0, " million dollars"}
   end
 
   koan "Round the number up or down for me" do
-    assert Float.round(5.5) == ___
-    assert Float.round(5.4) == ___
-    assert Float.round(8.94, 1) == ___
-    assert Float.round(-5.5674, 3) == ___
+    assert Float.round(5.5) == 6.0
+    assert Float.round(5.4) == 5.0
+    assert Float.round(8.94, 1) == 8.9
+    assert Float.round(-5.5674, 3) == -5.567
   end
 
   koan "I want the first and last in the range" do
     first..last = Range.new(1, 10)
 
-    assert first == ___
-    assert last == ___
+    assert first == 1
+    assert last == 10
   end
 
   koan "Does my number exist in the range?" do
     range = Range.new(1, 10)
 
-    assert 4 in range == ___
-    assert 10 in range == ___
-    assert 0 in range == ___
+    assert 4 in range == true
+    assert 10 in range == true
+    assert 0 in range == false
   end
 
-  koan "Is this a range?" do    
-    assert Range.range?(1..10) == ___
-    assert Range.range?(0) == ___
+  koan "Is this a range?" do
+    assert Range.range?(1..10) == true
+    assert Range.range?(0) == false
   end
 end
